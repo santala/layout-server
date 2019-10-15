d01b11128c81b85e833ceb08fea202e3a63c7917
santala <simo.a.santala@aalto.fi>
2019-10-15 15:01:42 +0300

    Started implementing grid model

--- optimizer/guidelines.py
+++ optimizer/guidelines.py
@@ -1,3 +1,4 @@
+from gurobipy import GRB, GenExpr, LinExpr, Model, tupledict, abs_, and_, max_, min_, QuadExpr, GurobiError
 
 
 def optimize():
@@ -7,7 +8,50 @@ def optimize():
         # Constrain rest to the remaining area (minus padding)
             # E.g. y0 limit must be max value of top edge elements y1
         # Define a grid (if reasonable)
-            # Check divisors
+
+    elem_ids = ['a', 'b', 'c']
+
+
+    m = Model('GLayoutQuality')
+
+    col_count = m.addVar(name='ColumnCount')
+    col_width = m.addVar(name='ColumnWidth')
+    gutter_width = m.addVar(name='GutterWidth')
+    grid_width = m.addVar(name='GridWidth')
+    row_count = m.addVar(name='RowCount')
+
+
+    col_start = m.addVars(elem_ids, vtype=GRB.INTEGER, lb=1, name='StartColumn')
+    col_end = m.addVars(elem_ids, vtype=GRB.INTEGER, lb=1, name='EndColumn')
+    m.addConstr((
+        col_end[e] >= col_start[e]
+        for e in elem_ids
+    ), name='ColumnStartEndOrder')
+
+    row_start = m.addVars(elem_ids, vtype=GRB.INTEGER, lb=1, name='RowColumn')
+    row_end = m.addVars(elem_ids, vtype=GRB.INTEGER, lb=1, name='RowColumn')
+    m.addConstr((
+        row_end[e] >= row_start[e]
+        for e in elem_ids
+    ), name='RowStartEndOrder')
+
+    # At least one element must start at the first column/row
+    m.addConstr(min_(col_start) == 1)
+    m.addConstr(min_(row_start) == 1)
+    # Bind column/row count
+    m.addConstr(col_count == max_(col_end))
+    m.addConstr(row_count == max_(row_end))
+
+    # All columns must fit within the available space
+    m.addConstr(grid_width + gutter_width >= col_width * col_count)
+
+    # OBJECTIVES
+
+    # Aim for best fit of grid
+    # minimize( layout_width + gutter_width - column_width * column_count )
+
+    # Minimize gaps in the grid (or maximize coverage)
+    # minimize( column_count * row_count - sum( col_span * row_span ) )
 
     pass
 
@@ -31,12 +75,13 @@ def optimize():
 # VARIABLES
 
 # General:
-    # column_count
+    # column_count == max(col_end)
     # column_width
     # gutter_width
     # layout_width
     # margin_left
     # margin_right
+    # row_count == max(row_end)
 
 # Per element:
     # col_start
@@ -55,10 +100,12 @@ def optimize():
 # min(col_start) == 1
 # min(row_start) == 1
 
-# At least one element must end at the last column
-# max(col_end) == column_count
+# TODO One element must be in top left corner
 
 # OBJECTIVES
 
 # Aim for best fit of grid
 # minimize( layout_width + gutter_width - column_width * column_count )
+
+# Minimize gaps in the grid (or maximize coverage)
+# minimize( column_count * row_count - sum( col_span * row_span ) )
